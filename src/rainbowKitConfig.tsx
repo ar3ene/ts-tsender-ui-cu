7e31f28db8ead2b1dbadf688b9e3b146ef317dcb
yingke <7677626+eigenbit@user.noreply.gitee.com>
2025-09-18 17:34:59 +0800

    fix: update rainbowKit configuration to include HTTP transport for Anvil

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
+import { http } from "@wagmi/core"
 import { arbitrum, base, mainnet, optimism, anvil, zksync, sepolia } from "wagmi/chains"
 
 export default getDefaultConfig({
     appName: "TSender",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
     chains: [mainnet, optimism, arbitrum, base, zksync, sepolia, anvil],
+    transports: {
+        [anvil.id]: http("http://127.0.0.1:8545"),
+    },
     ssr: false,
 })
