c0bb9977ceef5c909e1811a1c8f67260084340b8
yingke <7677626+eigenbit@user.noreply.gitee.com>
2025-09-18 17:51:12 +0800

    fix: refactor rainbowKit configuration to conditionally include Anvil transport

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -4,12 +4,12 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit"
 import { http } from "@wagmi/core"
 import { arbitrum, base, mainnet, optimism, anvil, zksync, sepolia } from "wagmi/chains"
 
+const isDev = process.env.NODE_ENV !== "production"
+
 export default getDefaultConfig({
     appName: "TSender",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
     chains: [mainnet, optimism, arbitrum, base, zksync, sepolia, anvil],
-    transports: {
-        [anvil.id]: http("http://127.0.0.1:8545"),
-    },
+    transports: isDev ? { [anvil.id]: http("http://127.0.0.1:8545") } : undefined,
     ssr: false,
 })
